87d9ecc3a23a7da0760ca07ba5c938aefdf5ac1f
Codinglone <codinglone@gmail.com>
2023-11-30 03:31:49 +0200

    Initial commit

---
+++ live-polls-app-backend/src/test/java/codinglone/livepollsappbackend/LivePollsAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package codinglone.livepollsappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivePollsAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
