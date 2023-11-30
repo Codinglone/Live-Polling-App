6f453432b1b4dce25f86f007ea9d11dea6424320
Codinglone <codinglone@gmail.com>
2023-11-30 10:50:49 +0200

    Added new project for backend side

---
+++ live-polls/src/main/java/com/codinglone/livepolls/LivePollsApplication.java
@@ -0,0 +1,13 @@
+package com.codinglone.livepolls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivePollsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivePollsApplication.class, args);
+	}
+
+}
--- live-polls/src/test/java/com/codinglone/livepolls/LivePollsApplicationTests.java
+++ live-polls/src/test/java/com/codinglone/livepolls/LivePollsApplicationTests.java
@@ -1,10 +1,10 @@
-package codinglone.livepollsappbackend;
+package com.codinglone.livepolls;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LivePollsAppBackendApplicationTests {
+class LivePollsApplicationTests {
 
 	@Test
 	void contextLoads() {
