6968b9f0f5dc22efe06e8c0770ad8bbd2fee1695
Codinglone <codinglone@gmail.com>
2023-12-01 03:24:43 +0200

    Finished setting up basic anonymous user UIs

---
+++ live-polls-app-frontend/src/components/AnonymousPolls.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { AiOutlineLike } from "react-icons/ai";
+import { AiOutlineDislike } from "react-icons/ai";
+const AnonymousPolls = () => {
+  return (
+    <div className="bg-[#F7F7F7] h-[85vh] w-full">
+      <div className="px-48 flex justify-between w-full">
+        <div className="w-full flex flex-col my-8">
+          
+        <div className="w-full bg-white shadow rounded py-4 px-4 flex flex-col">
+            <p className="text-[#696F79] text-lg">
+              Elephant are the largest animals in the world
+            </p>
+            <div className="flex justify-between w-[4vw] items-center my-2">
+              <span className="text-xl font-bold"><AiOutlineLike /></span>
+              <span className="text-xl font-bold"><AiOutlineDislike /></span>
+            </div>
+          </div>
+
+          <div className="w-full bg-white shadow rounded py-4 mt-6 px-4 flex flex-col">
+            <p className="text-[#696F79] text-lg">
+              Elephant are the largest animals in the world
+            </p>
+            <div className="flex justify-between w-[4vw] items-center my-2">
+              <span className="text-xl font-bold"><AiOutlineLike /></span>
+              <span className="text-xl font-bold"><AiOutlineDislike /></span>
+            </div>
+          </div>
+
+          <div className="w-full bg-white shadow rounded py-4 mt-6 px-4 flex flex-col">
+            <p className="text-[#696F79] text-lg">
+              Elephant are the largest animals in the world
+            </p>
+            <div className="flex justify-between w-[4vw] items-center my-2">
+              <span className="text-xl font-bold"><AiOutlineLike /></span>
+              <span className="text-xl font-bold"><AiOutlineDislike /></span>
+            </div>
+          </div>
+         
+
+          
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AnonymousPolls;
--- live-polls-app-frontend/src/pages/Anonymous.jsx
+++ live-polls-app-frontend/src/pages/Anonymous.jsx
@@ -3,13 +3,15 @@ import AnonymousOne from '../components/AnonymousOne'
 import AnonymousTwo from '../components/AnonymousTwo'
 import AnonymousThree from '../components/AnonymousThree'
 import AnonymousNavbar from '../components/AnonymousNavbar'
+import AnonymousPolls from '../components/AnonymousPolls'
 const Anonymous = () => {
   return (
     <div>
       <AnonymousNavbar />
         {/* <AnonymousOne /> */}
         {/* <AnonymousTwo /> */}
-        <AnonymousThree />
+        {/* <AnonymousThree /> */}
+        <AnonymousPolls />
     </div>
   )
 }
