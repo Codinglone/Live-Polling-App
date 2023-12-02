5bf23caffc9a238ebf17fdaca758b258fae82dab
Codinglone <codinglone@gmail.com>
2023-12-02 17:37:18 +0200

    Added functionality to show the popular words in the admin page

--- live-polls-app-frontend/src/components/AdminPolls.jsx
+++ live-polls-app-frontend/src/components/AdminPolls.jsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
 import { enqueueSnackbar } from "notistack";
 import axios from "axios";
-const AdminPolls = ({handlePollDetails}) => {
+
+const AdminPolls = ({ handlePollDetails }) => {
   const [polls, setPolls] = useState([]);
+  const [popularWords, setPopularWords] = useState([]);
 
   const baseUrl = "http://localhost:8080";
 
@@ -11,43 +13,66 @@ const AdminPolls = ({handlePollDetails}) => {
       // Make a DELETE request to delete the poll
       await axios.delete(`${baseUrl}/polls/delete/${poll.id}`);
 
-      enqueueSnackbar('Poll ended successfully', {
-        variant: 'success',
+      enqueueSnackbar("Poll ended successfully", {
+        variant: "success",
       });
 
       setTimeout(() => {
         window.location.reload();
       }, 2000);
     } catch (error) {
-      console.error('Error ending poll:', error);
-      enqueueSnackbar('Error ending poll', {
-        variant: 'error',
+      console.error("Error ending poll:", error);
+      enqueueSnackbar("Error ending poll", {
+        variant: "error",
       });
       // Handle errors, show a message, etc.
     }
+  };
+
+  const extractWords = (question) => {
+    const words = question.split(/\s+/);
+    return words.map((word) => word.toLowerCase());
+  };
+
+  const calculatePopularWords = () => {
+    const allWords = polls.reduce((words, poll) => {
+      const pollWords = extractWords(poll.question);
+      return [...words, ...pollWords];
+    }, []);
+
+    const wordCountMap = new Map();
+
+    allWords.forEach((word) => {
+      const count = wordCountMap.get(word) || 0;
+      wordCountMap.set(word, count + 1);
+    });
+
+    const sortedWords = Array.from(wordCountMap.entries())
+      .sort((a, b) => b[1] - a[1])
+      .map(([word]) => word);
+
+    setPopularWords(sortedWords.slice(0, 5)); // Display the top 5 popular words
   };
 
   useEffect(() => {
     axios
       .get(`${baseUrl}/polls`)
       .then(function (response) {
-        // handle success
-        console.log(response);
         setPolls(response.data);
+        calculatePopularWords();
       })
       .catch(function (error) {
-        // handle error
         console.log(error);
         enqueueSnackbar(error.message, {
           variant: "error",
         });
       });
   }, []);
+
   return (
     <div className="bg-[#F7F7F7] h-[85vh] w-full">
       <div className="px-48 flex justify-between w-full">
         <div className="w-2/3 flex flex-col my-8">
-        
           {polls.length >= 1 &&
             polls.map((poll) => (
               <div
@@ -55,33 +80,30 @@ const AdminPolls = ({handlePollDetails}) => {
                 onClick={() => handlePollDetails(poll)}
                 className="w-full bg-white shadow rounded py-4 px-4 mt-10 flex flex-col"
               >
-                <p className="text-[#696F79] text-lg">
-              {poll.question}
-            </p>
-            <div className="flex justify-between my-2">
-              <span className="text-xl font-bold">{poll.totalVotes}</span>
-              <button
-              onClick={() => handleEndPoll(poll)}
-               className="bg-[#0066F7] py-2 px-4 text-white rounded"
-               >
-                End Poll
-              </button>
-            </div>
-               
+                <p className="text-[#696F79] text-lg">{poll.question}</p>
+                <div className="flex justify-between my-2">
+                  <span className="text-xl font-bold">{poll.totalVotes}</span>
+                  <button
+                    onClick={() => handleEndPoll(poll)}
+                    className="bg-[#0066F7] py-2 px-4 text-white rounded"
+                  >
+                    End Poll
+                  </button>
+                </div>
               </div>
-            )) }
-          
+            ))}
         </div>
         <div className="w-1/3 mx-4 my-8">
-          <div className="w-full flex flex-col bg-white rounded shadow">
+          <div className="w-full flex flex-col bg-white rounded shadow" id="popular">
             <span className="mx-4 bg-[#007BF6] py-1 mx-8 text-center rounded text-lg text-white mt-4 font-semibold">
-              popular words
+              Popular Words
             </span>
             <div className="flex flex-col justify-center items-center">
-                <span className="my-2 text-md font-light">Elephant</span>
-                <span className="my-1 text-md font-light">Largest</span>
-                <span className="my-1 text-md font-light">World</span>
-                <span className="my-1 text-md font-light">are</span>
+              {popularWords.map((word, index) => (
+                <span key={index} className="my-2 text-md font-light">
+                  {word}
+                </span>
+              ))}
             </div>
           </div>
         </div>
@@ -91,3 +113,4 @@ const AdminPolls = ({handlePollDetails}) => {
 };
 
 export default AdminPolls;
+
--- live-polls-app-frontend/src/components/AdminQuizResult.jsx
+++ live-polls-app-frontend/src/components/AdminQuizResult.jsx
@@ -1,52 +1,5 @@
-// import React from 'react'
-
-// const AdminQuizResult = () => {
-//   return (
-//     <div className="bg-[#F7F7F7] h-[85vh] pt-24 w-full">
-//         <div className='px-64 w-full'>
-//             <table className='shadow bg-white border-2 w-full border-gray-300'>
-//                 <thead>
-//                     <tr>
-//                         <th className='uppercase py-4 px-4'  style={{border: "1px solid black"}}>Names</th>
-//                         <th className='uppercase py-4 px-4' style={{border: "1px solid black"}}>Result</th>
-//                     </tr>
-//                 </thead>
-//                 <tbody>
-//                     <tr>
-//                         <td style={{border: "1px solid black"}} className='w-1/2 px-4 py-4'>NIYOKWIZERWA Fabrice</td>
-//                         <td style={{border: "1px solid black"}} className='w-1/2 px-4 py-4'>80%</td>
-//                     </tr>
-//                     <tr>
-//                         <td style={{border: "1px solid black"}} className='w-1/2 px-4 py-4'>NIYOKWIZERWA Fabrice</td>
-//                         <td style={{border: "1px solid black"}} className='w-1/2 px-4 py-4'>80%</td>
-//                     </tr>
-//                     <tr>
-//                         <td style={{border: "1px solid black"}} className='w-1/2 px-4 py-4'>NIYOKWIZERWA Fabrice</td>
-//                         <td style={{border: "1px solid black"}} className='w-1/2 px-4 py-4'>80%</td>
-//                     </tr>
-//                     <tr>
-//                         <td style={{border: "1px solid black"}} className='w-1/2 px-4 py-4'>NIYOKWIZERWA Fabrice</td>
-//                         <td style={{border: "1px solid black"}} className='w-1/2 px-4 py-4'>80%</td>
-//                     </tr>
-//                     <tr>
-//                         <td style={{border: "1px solid black"}} className='w-1/2 px-4 py-4'>NIYOKWIZERWA Fabrice</td>
-//                         <td style={{border: "1px solid black"}} className='w-1/2 px-4 py-4'>80%</td>
-//                     </tr>
-//                     <tr>
-//                         <td style={{border: "1px solid black"}} className='w-1/2 px-4 py-4'>NIYOKWIZERWA Fabrice</td>
-//                         <td style={{border: "1px solid black"}} className='w-1/2 px-4 py-4'>80%</td>
-//                     </tr>
-//                 </tbody>
-//             </table>
-//         </div>
-//     </div>
-//   )
-// }
-
-// export default AdminQuizResult
-
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const AdminQuizResult = () => {
   const [questions, setQuestions] = useState([]);
@@ -54,14 +7,15 @@ const AdminQuizResult = () => {
 
   useEffect(() => {
     // Fetch quiz results from the endpoint
-    axios.get('http://localhost:8080/questions')
+    axios
+      .get("http://localhost:8080/questions")
       .then((response) => {
         // Assuming the response.data is an array of quiz questions
         setQuestions(response.data);
         calculateUserScores(response.data);
       })
       .catch((error) => {
-        console.error('Error fetching quiz questions:', error);
+        console.error("Error fetching quiz questions:", error);
       });
   }, []); // Empty dependency array ensures the effect runs once after the initial render
 
@@ -69,12 +23,15 @@ const AdminQuizResult = () => {
     const scoresMap = new Map();
 
     quizQuestions.forEach((question) => {
-      const userVotes = JSON.parse(question.userVotes || '[]');
+      const userVotes = JSON.parse(question.userVotes || "[]");
 
       userVotes.forEach((vote) => {
         const username = vote.username;
         const choice = vote.choice;
-        const isCorrect = JSON.parse(question.answers || '[]').find(answer => answer.text === choice)?.isCorrect || false;
+        const isCorrect =
+          JSON.parse(question.answers || "[]").find(
+            (answer) => answer.text === choice
+          )?.isCorrect || false;
 
         // If the user doesn't have an entry in scoresMap, initialize it
         if (!scoresMap.has(username)) {
@@ -102,15 +59,35 @@ const AdminQuizResult = () => {
         <table className="shadow bg-white border-2 w-full border-gray-300">
           <thead>
             <tr>
-              <th className="uppercase py-4 px-4" style={{ border: '1px solid black' }}>Username</th>
-              <th className="uppercase py-4 px-4" style={{ border: '1px solid black' }}>Score</th>
+              <th
+                className="uppercase py-4 px-4"
+                style={{ border: "1px solid black" }}
+              >
+                Username
+              </th>
+              <th
+                className="uppercase py-4 px-4"
+                style={{ border: "1px solid black" }}
+              >
+                Score
+              </th>
             </tr>
           </thead>
           <tbody>
             {userScores.map((user, index) => (
               <tr key={index}>
-                <td style={{ border: '1px solid black' }} className="w-1/2 px-4 py-4">{user.username}</td>
-                <td style={{ border: '1px solid black' }} className="w-1/2 px-4 py-4">{user.score}</td>
+                <td
+                  style={{ border: "1px solid black" }}
+                  className="w-1/2 px-4 py-4"
+                >
+                  {user.username}
+                </td>
+                <td
+                  style={{ border: "1px solid black" }}
+                  className="w-1/2 px-4 py-4"
+                >
+                  {user.score}
+                </td>
               </tr>
             ))}
           </tbody>
